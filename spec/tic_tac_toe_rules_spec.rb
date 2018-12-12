9208881765f0930ea4ddb6065faa7238dafc0edc
Tiffani Elmore <tiffanielmore@Tiffanis-MacBook-Pro.local>
2018-12-12 12:59:48 -0600

    add more expectations to test and refactor/DRY

--- spec/tic_tac_toe_rules_spec.rb
+++ spec/tic_tac_toe_rules_spec.rb
@@ -13,17 +13,12 @@ describe TicTacToeRules do
         it "should return true if a different set of markings STRAIGHT--match a winning combo" do
             tic_tac_toe_ruling = TicTacToeRules.new()
 
-            player_stats = [3,4,5]
-            player_win = tic_tac_toe_ruling.winner?(player_stats)
-            expect(player_win).to eq(true)
-        end
-
-        it "should return true if a different set of markings STRAIGHT-match a winning combo" do
-            tic_tac_toe_ruling = TicTacToeRules.new()
-
-            player_stats = [6,7,8]
-            player_win = tic_tac_toe_ruling.winner?(player_stats)
-            expect(player_win).to eq(true)
+            player = tic_tac_toe_ruling
+            expect(player.winner?([3,4,5])).to eq(true)
+            expect(player.winner?([6,7,8])).to eq(true)
+            expect(player.winner?([0,3,6])).to eq(true)
+            expect(player.winner?([1,4,7])).to eq(true)
+            expect(player.winner?([2,5,8])).to eq(true)
         end
 
         it "should return true if a set of markings BOX-match a winning combo" do
@@ -34,12 +29,25 @@ describe TicTacToeRules do
             expect(player_win).to eq(true)
         end
 
+        it "should return true if a different set of markings BOX-match a winning combo" do
+            tic_tac_toe_ruling = TicTacToeRules.new()
+
+            player = tic_tac_toe_ruling
+            expect(player.winner?([3,5,4])).to eq(true)
+            expect(player.winner?([8,6,7])).to eq(true)
+            expect(player.winner?([3,6,0])).to eq(true)
+            expect(player.winner?([4,7,1])).to eq(true)
+            expect(player.winner?([2,8,5])).to eq(true)
+        end
+
         it "should return true if a set of markings CONTAIN a winning combo" do
             tic_tac_toe_ruling = TicTacToeRules.new()
 
-            player_stats = [7,6,3,0]
-            player_win = tic_tac_toe_ruling.winner?(player_stats)
-            expect(player_win).to eq(true)
+            player = tic_tac_toe_ruling
+            expect(player.winner?([7,6,3,0])).to eq(true)
+            expect(player.winner?([1,4,7,2])).to eq(true)
+            expect(player.winner?([0,6,3,4])).to eq(true)
+            expect(player.winner?([8,4,3,0])).to eq(true)
         end
 
 
