8508b59d98503535153f38feed78503bdaca55f4
Tiffani Elmore <tiffanielmore@Tiffanis-MacBook-Pro.local>
2018-12-12 15:08:23 -0600

    rename test conditions and remove win_combo from winner method

--- lib/tic_tac_toe_rules.rb
+++ lib/tic_tac_toe_rules.rb
@@ -1,17 +1,8 @@
 class TicTacToeRules
     def winner?(player_stats)
+        return false if player_stats.length <= 2 
         player_stats = player_stats.sort()
         match_box = []
-        winning_combos = [
-            [0,1,2],
-            [3,4,5],
-            [6,7,8],
-            [0,3,6],
-            [1,4,7],
-            [2,5,8],
-            [0,4,8],
-            [2,4,6]
-        ]
 
         winning_combos.map do |combo|
             match_box = []
@@ -22,5 +13,18 @@ class TicTacToeRules
             end
             return true if match_box.length == 3
         end
+    end
+
+    def winning_combos
+        [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ]
     end
 end
--- spec/tic_tac_toe_rules_spec.rb
+++ spec/tic_tac_toe_rules_spec.rb
@@ -2,52 +2,55 @@ require "tic_tac_toe_rules"
 
 describe TicTacToeRules do
     describe "#winner?" do
-        it "should return true if a set of markings STRAIGHT--match a winning combo" do
-            tic_tac_toe_ruling = TicTacToeRules.new()
+        it "should return true if a set of player markings STRAIGHT--match a winning combo" do
+            result = TicTacToeRules.new()
 
             player_stats = [0,1,2]
-            player_win = tic_tac_toe_ruling.winner?(player_stats)
-            expect(player_win).to eq(true)
+            call = result.winner?(player_stats)
+            expect(call).to eq(true)
         end
 
         it "should return true if a different set of markings STRAIGHT--match a winning combo" do
-            tic_tac_toe_ruling = TicTacToeRules.new()
-
-            player = tic_tac_toe_ruling
-            expect(player.winner?([3,4,5])).to eq(true)
-            expect(player.winner?([6,7,8])).to eq(true)
-            expect(player.winner?([0,3,6])).to eq(true)
-            expect(player.winner?([1,4,7])).to eq(true)
-            expect(player.winner?([2,5,8])).to eq(true)
+            result = TicTacToeRules.new()
+            
+            expect(result.winner?([3,4,5])).to eq(true)
+            expect(result.winner?([6,7,8])).to eq(true)
+            expect(result.winner?([0,3,6])).to eq(true)
+            expect(result.winner?([1,4,7])).to eq(true)
+            expect(result.winner?([2,5,8])).to eq(true)
         end
 
         it "should return true if a set of markings BOX-match a winning combo" do
-            tic_tac_toe_ruling = TicTacToeRules.new()
+            result = TicTacToeRules.new()
 
             player_stats = [7,6,8]
-            player_win = tic_tac_toe_ruling.winner?(player_stats)
-            expect(player_win).to eq(true)
+            call = result.winner?(player_stats)
+            expect(call).to eq(true)
         end
 
         it "should return true if a different set of markings BOX-match a winning combo" do
-            tic_tac_toe_ruling = TicTacToeRules.new()
+            result = TicTacToeRules.new()
 
-            player = tic_tac_toe_ruling
-            expect(player.winner?([3,5,4])).to eq(true)
-            expect(player.winner?([8,6,7])).to eq(true)
-            expect(player.winner?([3,6,0])).to eq(true)
-            expect(player.winner?([4,7,1])).to eq(true)
-            expect(player.winner?([2,8,5])).to eq(true)
+            expect(result.winner?([3,5,4])).to eq(true)
+            expect(result.winner?([8,6,7])).to eq(true)
+            expect(result.winner?([3,6,0])).to eq(true)
+            expect(result.winner?([4,7,1])).to eq(true)
+            expect(result.winner?([2,8,5])).to eq(true)
         end
 
         it "should return true if a set of markings CONTAIN a winning combo" do
-            tic_tac_toe_ruling = TicTacToeRules.new()
+            result = TicTacToeRules.new()
 
-            player = tic_tac_toe_ruling
-            expect(player.winner?([7,6,3,0])).to eq(true)
-            expect(player.winner?([1,4,7,2])).to eq(true)
-            expect(player.winner?([0,6,3,4])).to eq(true)
-            expect(player.winner?([8,4,3,0])).to eq(true)
+            expect(result.winner?([7,6,3,0])).to eq(true)
+            expect(result.winner?([1,4,7,2])).to eq(true)
+            expect(result.winner?([0,6,3,4])).to eq(true)
+            expect(result.winner?([8,4,3,0])).to eq(true)
+        end
+
+        it "should return false if a set of markings does not contain at least 3 marks" do
+            result = TicTacToeRules.new()
+
+            expect(result.winner?([0,4])).to eq(false)
         end
 
 
