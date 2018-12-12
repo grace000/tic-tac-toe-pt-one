b38d194103a84ac224a7b8276bfb4ba163b18d83
Tiffani Elmore <tiffanielmore@Tiffanis-MacBook-Pro.local>
2018-12-12 11:53:12 -0600

    initial TicTacToeRules class and winner method

---
+++ lib/tic_tac_toe_rules.rb
@@ -0,0 +1,18 @@
+class TicTacToeRules
+    def winner?(player_stats)
+        winning_combos = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ]
+
+        winning_combos.map do |combo|
+            return true if player_stats == combo
+        end
+    end
+end
---
+++ spec/tic_tac_toe_rules_spec.rb
@@ -0,0 +1,31 @@
+require "tic_tac_toe_rules"
+
+describe TicTacToeRules do
+    describe "#winner?" do
+        it "should return true if a player's set of markings straight-match a winning combo" do
+            tic_tac_toe_ruling = TicTacToeRules.new()
+
+            player_stats = [0,1,2]
+            player_win = tic_tac_toe_ruling.winner?(player_stats)
+            expect(player_win).to eq(true)
+        end
+
+        it "should return true if a different player's set of markings straight-match a winning combo" do
+            tic_tac_toe_ruling = TicTacToeRules.new()
+
+            player_stats = [3,4,5]
+            player_win = tic_tac_toe_ruling.winner?(player_stats)
+            expect(player_win).to eq(true)
+        end
+
+        it "should return true if a different player's set of markings straight-match a winning combo" do
+            tic_tac_toe_ruling = TicTacToeRules.new()
+
+            player_stats = [6,7,8]
+            player_win = tic_tac_toe_ruling.winner?(player_stats)
+            expect(player_win).to eq(true)
+        end
+
+    end  
+    
+end
