f65684c374a95d1b5c52e049c3e803a63e823197
Tiffani Elmore <tiffanielmore@Tiffanis-MacBook-Pro.local>
2018-12-18 12:33:23 -0600

    private win combos and refactor rules class

--- lib/board.rb
+++ lib/board.rb
@@ -1,65 +1,19 @@
 
 class Board
-    def spaces(current_game_type)
-        if current_game_type == "Tic Tac Toe"
-            Array.new(9, " ")
-        end
+    
+    def initialize
+        @moves = []
     end
     
-    def board_full?(current_game_type, current_game_state)
-        if current_game_type == "Tic Tac Toe"
-            if current_game_state.include? (" ")
-                return false
-            end
-            true
-        end
+    def empty?
+        @moves.length == 0
     end
 
-    def empty_spaces_locations(current_game_type, current_game_state)
-        locations = []
-        if current_game_type == "Tic Tac Toe"
-            current_game_state.map.with_index do |value, index|
-               if value == " "
-                locations << index
-               end
-            end
-        end
-        locations
+    def full?
+        @moves.length == 9
     end
 
-    def position_available?(current_game_type, current_game_state, requested_space)
-        if current_game_type == "Tic Tac Toe"
-            return true if current_game_state[requested_space] == " "
-        end
-        false
+    def move(position)
+        @moves << position
     end
-
-    def fill_space(current_game_type, current_game_state, requested_move, player_marker)
-        if current_game_type == "Tic Tac Toe"
-            if current_game_state[requested_move] == " "
-               current_game_state[requested_move] = player_marker
-               current_game_state
-            end
-        end
-    end
-
-    def player_positions(current_game_type, current_game_state)
-        player_spaces = []
-        
-        if current_game_type == "Tic Tac Toe"
-            player_x = ["X",[]]
-            player_o = ["O",[]]
-
-            player_spaces = [player_x, player_o]
-            current_game_state.map.with_index do |value, index|
-                if value == "X"
-                    player_spaces[0][1] << index
-                elsif value == "O"
-                    player_spaces[1][1] << index
-                end
-            end
-        end
-        player_spaces
-    end
-
 end
--- spec/game_results_spec.rb
+++ spec/game_results_spec.rb
@@ -1,9 +1,10 @@
-require "tic_tac_toe_rules"
+require "game_results"
+require 'board'
 
-describe TicTacToeRules do
+describe GameResults do
     describe "#winner?" do
         it "should return true if a set of markings STRAIGHT--match a winning combo" do
-            result = TicTacToeRules.new()
+            result = GameResults.new()
 
             moves = [0,1,2]
             call = result.winner?(moves)
@@ -11,7 +12,7 @@ describe TicTacToeRules do
         end
 
         it "should return true if a different set of markings STRAIGHT--match a winning combo" do
-            result = TicTacToeRules.new()
+            result = GameResults.new()
             
             expect(result.winner?([3,4,5])).to eq(true)
             expect(result.winner?([6,7,8])).to eq(true)
@@ -21,7 +22,7 @@ describe TicTacToeRules do
         end
 
         it "should return true if a set of markings BOX-match a winning combo" do
-            result = TicTacToeRules.new()
+            result = GameResults.new()
 
             moves = [7,6,8]
             call = result.winner?(moves)
@@ -29,7 +30,7 @@ describe TicTacToeRules do
         end
 
         it "should return true if a different set of markings BOX-match a winning combo" do
-            result = TicTacToeRules.new()
+            result = GameResults.new()
 
             expect(result.winner?([3,5,4])).to eq(true)
             expect(result.winner?([8,6,7])).to eq(true)
@@ -39,7 +40,7 @@ describe TicTacToeRules do
         end
 
         it "should return true if a set of markings CONTAIN a winning combo" do
-            result = TicTacToeRules.new()
+            result = GameResults.new()
 
             expect(result.winner?([7,6,3,0])).to eq(true)
             expect(result.winner?([1,4,7,2])).to eq(true)
@@ -48,7 +49,7 @@ describe TicTacToeRules do
         end
 
         it "should return false if a set of markings does not contain at least 3 marks" do
-            result = TicTacToeRules.new()
+            result = GameResults.new()
 
             expect(result.winner?([0,4])).to eq(false)
             expect(result.winner?([0])).to eq(false)
@@ -56,27 +57,62 @@ describe TicTacToeRules do
     end 
     
     describe "#draw?" do
-        it "should return false if a set of markings does not contain at least 4 marks" do
-            result = TicTacToeRules.new()
+        it "returns false if the board is empty" do 
+            board = Board.new
+            result = GameResults.new
 
-            expect(result.draw?([0])).to eq(false)
-            expect(result.draw?([0,4])).to eq(false)
-            expect(result.draw?([0,4,3])).to eq(false)
+            expect(result.draw?(board)).to eq(false)
         end
 
-        it "should return false if a set of markings contain a winning combo" do
-            result = TicTacToeRules.new()
+        it "returns true if the board is full" do
+            board = Board.new
+            result = GameResults.new
+            
+            board.move(0)
+            board.move(1)
+            board.move(2)
+            board.move(3)
+            board.move(4)
+            board.move(5)
+            board.move(6)
+            board.move(7)
+            board.move(8)
 
-            expect(result.draw?([0,2,4,6])).to eq(false)
-            expect(result.draw?([7,6,3,0])).to eq(false)
+            expect(result.draw?(board)).to eq(true)
         end
 
-        it "should return true if a set of markings does not contain a winning combo" do
-            result = TicTacToeRules.new()
+        it "returns false if the board is not full" do
+            board = Board.new
+            result = GameResults.new
 
-            expect(result.draw?([0,5,4,6])).to eq(true)
-            expect(result.draw?([1,5,8,6])).to eq(true)
+            board.move(4)
+            board.move(5)
+            board.move(6)
+            board.move(7)
+            board.move(8)
+
+            expect(result.draw?(board)).to eq(false)
         end
     end 
+    
+    describe "#winning_combos" do 
+        it "should return an array with winning rows,cols,diags" do
+            result = GameResults.new()
+            
+            combo_array = [
+                [0,1,2],
+                [3,4,5],
+                [6,7,8],
+                [0,3,6],
+                [1,4,7],
+                [2,5,8],
+                [0,4,8],
+                [2,4,6]
+            ]
+
+            expect(result.winning_combos).to eq(combo_array)
+        end
+    
+    end
     
 end
