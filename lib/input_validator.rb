76932d28eaa6a3e999da39c21c394c8707a60430
Tiffani Elmore <tiffanielmore@Tiffanis-MacBook-Pro.local>
2018-12-19 11:10:54 -0600

    check that move requests are valid integers

--- lib/input_validator.rb
+++ lib/input_validator.rb
@@ -1,4 +1,9 @@
 class InputValidator
-    def valid_character?
+    def valid_character?(move)
+        regex = /\D/ 
+        if move =~ regex || move <= 0
+            return false
+        end
+        true
     end
 end
--- spec/input_validator_spec.rb
+++ spec/input_validator_spec.rb
@@ -2,10 +2,15 @@ require "input_validator"
 
 describe InputValidator do
     describe "#valid_character?" do
-        it "should return true for X and O characters" do
+        it "should return true for integers 1 through 9" do
             input_v = InputValidator.new
 
-            
+            expect(input_v.valid_character?(2)).to eq(true)
+            expect(input_v.valid_character?(5)).to eq(true)
+            expect(input_v.valid_character?(0)).to eq(false)
+            expect(input_v.valid_character?("k")).to eq(false)
+            expect(input_v.valid_character?("+")).to eq(false)
+            expect(input_v.valid_character?("/")).to eq(false)
         end     
     end
 end
