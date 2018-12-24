c0f42f0ac152c9a79cd8dec4fac7d9cdf48b1a2a
Tiffani <tiffani.grace.elmore@gmail.com>
2018-12-24 15:24:29 -0600

    remove board and prompt methods

--- spec/game-engine_spec.rb
+++ spec/game-engine_spec.rb
@@ -5,21 +5,7 @@ describe GameEngine do
         @game = GameEngine.new
     end
 
-    describe "#display_board" do
-        it "displays a board with no player marks" do
-
-            expect(@game.display_board).to eq(
-                <<~HEREDOC
-                    1    |  2   |   3   
-                --------------------------
-                    4    |  5   |   6   
-                --------------------------
-                    7    |  8   |   9  
-                HEREDOC
-            )
-        end
-    end
-
     describe "#start" do
+        
     end
 end
