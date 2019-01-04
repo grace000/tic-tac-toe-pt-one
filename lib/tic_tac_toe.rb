7405e82c4170296473b89d925881572f387401f9
Tiffani <tiffani.grace.elmore@gmail.com>
2019-01-04 11:22:15 -0600

    make selected token class variable

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -5,6 +5,7 @@ require_relative './board'
 
 class TicTacToe
     attr_accessor :board, :board_presenter
+    @@selected_token
     def initialize
         @board = Board.new
         @board_presenter = BoardPresenter.new
@@ -14,16 +15,16 @@ class TicTacToe
         puts Prompt::WELCOME
         CommandLineIn.new.get_input
         puts Prompt::MAKE_TOKEN_SELECTION
+        @@selected_token = CommandLineIn.new.get_input
+        puts "Thanks for selecting #{@@selected_token}. Let's start the game!" 
     end
 
     def play
         welcome_player
-        selected_token = CommandLineIn.new.get_input
-        puts "Thanks for selecting #{selected_token}. Let's start the game!" 
         puts board_presenter.display_board(board.moves)
         puts Prompt::MAKE_COORDINATE_SELECTION
         selected_move = CommandLineIn.new.get_input.to_i
-        take_turn(selected_move, selected_token)
+        take_turn(selected_move, @@selected_token)
         puts board_presenter.display_board(board.moves)
     end
 
