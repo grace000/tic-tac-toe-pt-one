02bcb8edfa92ba9d566abe08ec95a9f19db33705
Tiffani <tiffani.grace.elmore@gmail.com>
2019-01-09 16:27:10 -0500

    remove commented out code

--- lib/board_presenter.rb
+++ lib/board_presenter.rb
@@ -1,7 +1,4 @@
 class BoardPresenter
-    # def display_board
-    #     "        |      |      #{horizontal_bar}        |      |      #{horizontal_bar}        |      |     \n"
-    # end
     def display_board(board)
         "    #{board[0]}    |   #{board[1]}   |    #{board[2]}  #{horizontal_bar}    #{board[3]}    |   #{board[4]}   |    #{board[5]}  #{horizontal_bar}    #{board[6]}    |   #{board[7]}   |   #{board[8]}  \n"
     end
