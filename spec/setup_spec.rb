67b0b0dba4211211c080c5a597805aa8b853edc8
Tiffani <tiffani.grace.elmore@gmail.com>
2019-02-05 17:11:37 -0600

    add assign computer token test

--- spec/setup_spec.rb
+++ spec/setup_spec.rb
@@ -17,5 +17,16 @@ describe Setup do
             
             expect(@setup.players[1].token).to eq("X")
         end
+
+        it "does not assign letter X as token when human token is X" do
+            
+            human_token = "X"
+            human_name = "Human"
+            human_player = Player.new(token:human_token, name:human_name)
+            @setup.players << human_player
+            @setup.assign_computer_token(human_token)
+            
+            expect(@setup.players[1].token).not_to eq("X")
+        end
     end
 end
