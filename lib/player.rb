61d735f99652c5de102ead991138eb844d2b4438
Tiffani <tiffani.grace.elmore@gmail.com>
2019-02-06 12:02:08 -0600

    add instance variable for player type

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,12 @@
 class Player 
-    attr_accessor :token, :name
+    attr_accessor :token, :name, :player
     def initialize(args)
         @token = args[:token]
         @name = args[:name]
+        @player = args[:player]
+    end
+
+    def selected_coordinate
+        player.select_coordinate
     end
 end
