56289f061562506a1a2233c89b91fcce7e0a5df7
Tiffani <tiffani.grace.elmore@gmail.com>
2019-02-28 11:47:04 -0600

    minimax is finally working?

--- lib/hard_computer_player.rb
+++ lib/hard_computer_player.rb
@@ -13,6 +13,8 @@ class HardComputerPlayer
         get_best_computer_move(board, players)
     end
 
+    private
+
     def get_best_computer_move(board, players)
         best_move_score = -1000
         best_move = nil
@@ -23,7 +25,7 @@ class HardComputerPlayer
                 best_move_score = current_score
                 best_move = current_move
             end
-            board.moves[current_move] = current_move
+            board.moves[current_move-1] = current_move
         end
         best_move
     end
@@ -43,7 +45,6 @@ class HardComputerPlayer
                 best_score = [best_score, get_best_move_score(board, players, depth+1, !is_maximizing_player)].max
                 board.moves[move-1] = move
             end
-            print best_score
             best_score
         else
             best_score = 1000
